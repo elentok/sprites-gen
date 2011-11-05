68e2a9cc1235cf489d9fc7007fc7f40455d7c3d9
David E <3david@gmail.com>
2011-11-05 17:40:25 +0200

    The 'combine' method is working + added .gitignore

--- sprites-gen.py
+++ sprites-gen.py
@@ -1,15 +1,34 @@
 #!/usr/bin/env python
 
+import os
 import argparse
 import Image
 
 def combine(input_files, output_file):
     """
-    >>> combine(['test/image1.png', 'test/image2.png'], 'test/tmp.output.png')
-    >>> Image.open('test/tmp.output.png').im_size
-    (384, 256)
+    >>> os.remove('test/tmp.output.png')
+    >>> combine(['test/image1.png', 'test/image2.png', 'test/image3.png'], 'test/tmp.output.png')
+    >>> image = Image.open('test/tmp.output.png')
+    >>> image.size
+    (448, 256)
+    >>> image.mode
+    'RGBA'
     """
     output_image_size = get_output_image_size(input_files)
+    mode = Image.open(input_files[0]).mode
+    sprites_image = Image.new(mode, output_image_size)
+
+    x = 0
+    for input_filename in input_files:
+        image = Image.open(input_filename)
+        (width, height) = image.size
+        box = (x, 0, x + width, height)
+        sprites_image.paste(image, box)
+        x += width
+
+    sprites_image.save('test/tmp.output.png')
+
+
 
 def get_output_image_size(input_files):
     """
